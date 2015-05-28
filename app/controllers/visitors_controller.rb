7b6f0dfb6690ba6df09549e8737643fe21bd71c8
lucywiswallft <lucy.wiswall@bipt.com>
2015-05-28 12:58:14 -0400

    front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -3,8 +3,6 @@ class VisitorsController < ApplicationController
   def new
 
     @owner = Owner.new
-    flash.now[:notices] = 'Welcome!'
-    flash.now[:alert] = 'My birthday is soon.'
   end
 
 end
